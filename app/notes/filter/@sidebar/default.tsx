280ed0578a6ba535978017659e8bfe2699364b6e
Vladimir <kremench@gmail.com>
2025-09-27 23:16:07 +0300

    add @modal

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,5 +1,11 @@
+import SidebarNotes from "@/components/SidebarNotes/SidebarNotes";
+
 const NotesSidebar = async () => {
-  return <div>NotesSidebar</div>;
+  return (
+    <div>
+      <SidebarNotes />
+    </div>
+  );
 };
 
 export default NotesSidebar;
